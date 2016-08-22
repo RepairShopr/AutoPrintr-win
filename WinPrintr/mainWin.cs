d7e791024418c73ce1cbc00ee84d1298c80a73ed
VoidVolker <voidvolker@gmail.com>
2016-08-22 20:38:26 +0300

    [+] Application prototype

---
+++ WinPrintr/mainWin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinPrintr
+{
+    public partial class mainWin : Form
+    {
+        public mainWin()
+        {
+            InitializeComponent();
+            this.printersTable.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+            
+        }
+    }
+}
