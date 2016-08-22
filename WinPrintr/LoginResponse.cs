0f5bf74b6a395a3363f8d0ef830a53d5abfabd28
VoidVolker <voidvolker@gmail.com>
2016-08-22 20:38:26 +0300

    [+] Application prototype

---
+++ WinPrintr/LoginResponse.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+
+namespace WinPrintr
+{
+    public class CustomerPermission
+    {
+        public bool read { get; set; }
+        public bool write { get; set; }
+        public bool delete { get; set; }
+    }
+
+    public class TicketPermission
+    {
+        public bool read { get; set; }
+        public bool write { get; set; }
+        public bool delete { get; set; }
+    }
+
+    public class InvoicePermission
+    {
+        public bool read { get; set; }
+        public bool write { get; set; }
+        public bool delete { get; set; }
+    }
+
+    public class Payment
+    {
+        public bool read { get; set; }
+        public bool write { get; set; }
+        public bool delete { get; set; }
+    }
+
+    public class Permissions
+    {
+        public CustomerPermission customer { get; set; }
+        public TicketPermission ticket { get; set; }
+        public InvoicePermission invoice { get; set; }
+        public Payment payment { get; set; }
+    }
+
+    public class LoginResponse
+    {
+        [JsonProperty("user_token")]
+        public string UserToken { get; set; }
+
+        [JsonProperty("user_email")]
+        public string UserEmail { get; set; }
+
+        [JsonProperty("user_name")]
+        public string UserName { get; set; }
+
+        [JsonProperty("user_id")]
+        public int UserId { get; set; }
+
+        [JsonProperty("admin")]
+        public bool Admin { get; set; }
+
+        [JsonProperty("can_use_app")]
+        public bool CanUseApp { get; set; }
+
+        [JsonProperty("Subdomain")]
+        public string Subdomain { get; set; }
+
+        [JsonProperty("Permissions")]
+        public Permissions Permissions { get; set; }
+    }
+}
---
+++ WinPrintr/Printers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinPrintr
+{
+    static class Printers
+    {
+        public static Printer[] get()
+        {
+            Printer[] list = null;
+            int i = 0;
+            foreach (string printer in System.Drawing.Printing.PrinterSettings.InstalledPrinters)
+            {
+                list[i++] = new Printer { name = printer };
+            }
+            return list;
+        }
+    }
+
+    struct Printer
+    {
+        public string name;
+        public string id;
+    }
+}
