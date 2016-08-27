cc009618acb60fd8d8ecc4589441f6aae392548a
VoidVolker <voidvolker@gmail.com>
2016-08-27 22:02:37 +0300

    [+] print enginges select, jobs tab

---
+++ AutoPrintr/JobsList.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace AutoPrintr
+{
+    partial class JobsList
+    {
+        /// <summary> 
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Обязательный метод для поддержки конструктора - не изменяйте 
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.table = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // table
+            // 
+            this.table.AutoSize = true;
+            this.table.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.table.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.table.ColumnCount = 1;
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.table.Dock = System.Windows.Forms.DockStyle.Top;
+            this.table.Location = new System.Drawing.Point(0, 0);
+            this.table.Name = "table";
+            this.table.RowCount = 1;
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.table.Size = new System.Drawing.Size(245, 2);
+            this.table.TabIndex = 0;
+            // 
+            // JobsList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Controls.Add(this.table);
+            this.Name = "JobsList";
+            this.Size = new System.Drawing.Size(245, 142);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel table;
+    }
+}
