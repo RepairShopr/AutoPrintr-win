561e5b059ef37597f8482433a0d48c6f87588057
VoidVolker <voidvolker@gmail.com>
2016-08-27 22:02:37 +0300

    [+] print enginges select, jobs tab

--- AutoPrintr/JobsList.cs
+++ AutoPrintr/JobsList.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,53 +10,159 @@ using System.Windows.Forms;
 
 namespace AutoPrintr
 {
-    //static class JobsList
-    //{
-    //    static List<jItem> items = new List<jItem>();
-    //    static public void init()
-    //    {
-    //        Program.window.jobsList.ColumnStyles.Clear();
-    //        Jobs.jobAdded += Jobs_jobAdded;
-    //    }
+    public partial class JobsList : UserControl
+    {
+        public Dictionary<int, JLJob> items = new Dictionary<int, JLJob>();
 
-    //    static void addJob(Job j)
-    //    {
-    //        var jlist = Program.window.jobsList;
-    //        jlist.Controls.Add(new jItem(j), 0, jlist.RowCount++);
-    //        jlist.RowStyles.Add(new RowStyle(SizeType.AutoSize));
-    //    }
+        void update(JLJob ji, Job j)
+        {
+            ji.update(j);
+        }
+        delegate void updateCb(JLJob ji, Job j);
 
-    //    static delegate void addJobCB(Job j);
 
-    //    static void Jobs_jobAdded(object sender, Job j)
-    //    {
-    //        Program.window.Invoke(new addJobCB(addJob), new object[] { j });
-    //    }
+        void addJobControls(JLJob ji)
+        {
+            table.RowCount++;
+            table.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+            table.Controls.Add(ji.lIndex, 0, ji.row);
+            table.Controls.Add(ji.lFile, 1, ji.row);
+            table.Controls.Add(ji.lState, 2, ji.row);
+            table.Controls.Add(ji.lProgress, 3, ji.row);
+            table.Controls.Add(ji.lRecived, 4, ji.row);
+            table.Controls.Add(ji.lType, 5, ji.row);
+            table.Controls.Add(ji.lDocument, 6, ji.row);
+            table.Controls.Add(ji.lPrinters, 7, ji.row);
+            table.Controls.Add(ji.lUrl, 8, ji.row);
+        }
+        public delegate void addJobControlsCb(JLJob ji);
 
-    //    class jItem : Label
-    //    {
-    //        public Job job;
-    //        public jItem(Job j)
-    //        {
-    //            Margin = new Padding(2, 3, 2, 0);
-    //            Padding = new Padding(0);
-    //            Text = j.ToString();
-    //            job = j;
-    //            items.Add(this);
-    //            j.downloaded += j_downloaded;
-    //            j.printed += j_printed;
-    //        }
+        public void add(Job j)
+        {
+            int row = table.RowCount;
+            JLJob ji = new JLJob(j, row, items.Count);
 
-    //        void j_printed(object sender, Job j)
-    //        {
-    //            Text = j.ToString();
-    //        }
+            if (InvokeRequired)
+            {
+                Invoke(new addJobControlsCb(this.addJobControls), new object[] { ji });
+            }
+            else
+            {
+                addJobControls(ji);
+            }
 
-    //        void j_downloaded(object sender, Job j)
-    //        {
-    //            Text = j.ToString();
-    //        }
-    //    }
-    //}
-    
+            j.onChange += (object sender, Job job) =>
+            {
+                if (InvokeRequired)
+                {
+                    Invoke(new updateCb(this.update), new object[] { ji, j });
+                }
+                else
+                {
+                    update(ji, j);
+                }
+            };
+
+            items.Add(row, ji);
+        }
+
+        void addColumn(string text)
+        {
+            table.ColumnCount++;
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+            table.Controls.Add(new JLHeaderLabel(text), table.ColumnCount-1, 0);
+        }
+
+        public JobsList()
+        {
+            InitializeComponent();
+            table.RowStyles.Clear();
+            table.Controls.Add(new JLHeaderLabel("№"), 0, 0);
+            addColumn("File");
+            addColumn("State");
+            addColumn("Progress");
+            addColumn("Recived");
+            addColumn("Type");
+            addColumn("Document");
+            addColumn("Printers");
+            addColumn("Url");
+        }
+
+        public class JLJob : Job
+        {
+            public Job job;
+            public int row;
+            public int index;
+            public JLLabel lIndex;
+            public JLLabel lFile;
+            public JLLabel lState;
+            public JLLabel lProgress;
+            public JLLabel lRecived;
+            public JLLabel lType;
+            public JLLabel lDocument;
+            public JLLabel lPrinters;
+            public JLLabel lUrl;
+
+            public JLJob(Job j, int row, int index)
+            {
+                job = j;
+                this.row = row;
+                this.index = index;
+                lIndex = new JLLabel(index.ToString());
+                lFile = new JLLabel(j.documentName);
+                lState = new JLLabel(j.state.ToString());
+                lProgress = new JLLabel(j.progress.ToString()+"%");
+                lRecived = new JLLabel(Util.BytesToString(j.recived));
+                lType = new JLLabel(j.type);
+                lDocument = new JLLabel(j.documentTitle);
+                lPrinters = new JLLabel(string.Join<Printer>(",", j.printers.ToArray()));
+                lUrl = new JLLabel(j.file);
+            }
+
+            public void update(Job j)
+            {
+                //lFile.Text = j.documentName.ToString();
+                lState.Text = j.state.ToString();
+                lProgress.Text = j.progress.ToString() + "%";
+                lRecived.Text = Util.BytesToString(j.recived);
+            }
+
+        }
+
+
+        public class JLLabel : Label
+        {
+            //public Job job;
+            //public JLLabel(string text, Job j)
+            public JLLabel(string text)
+            {
+                AutoEllipsis = true;
+                Text = text;
+                Padding = new Padding(3, 5, 3, 0);
+                Margin = new Padding(0, 0, 0, 0);
+                Dock = DockStyle.Fill;
+
+                // Create the ToolTip and associate with the Form container.
+                ToolTip tt = new ToolTip();
+                // Set up the delays for the ToolTip.
+                tt.AutoPopDelay = 5000;
+                tt.InitialDelay = 1000;
+                tt.ReshowDelay = 500;
+                // Force the ToolTip text to be displayed whether or not the form is active.
+                tt.ShowAlways = true;
+                tt.SetToolTip(this, text);
+
+            }
+        }
+
+        public class JLHeaderLabel : Label
+        {
+            public JLHeaderLabel(string text)
+            {
+                Text = text;
+                Padding = new Padding(3, 5, 3, 0);
+                Margin = new Padding(0, 0, 0, 0);
+            }
+        }
+    }
 }
---
+++ AutoPrintr/PrintEngineDD.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoPrintr
+{
+    class PrintEngineDD : ComboBox
+    {
+        public Printer printer;
+        public PrintEngineDD(Printer printer)
+        {
+            this.printer = printer;
+            DropDownStyle = ComboBoxStyle.DropDownList;
+            foreach (var engine in Enum.GetValues(typeof(PrintEngine)))
+            { 
+                Items.Add(engine.ToString());
+            }
+            this.SelectedIndexChanged += PrintEngineDD_TextChanged;
+            this.Text = printer.printEngine.ToString();
+        }
+
+        void PrintEngineDD_TextChanged(object sender, EventArgs e)
+        {
+            printer.printEngine = PrintEngines.type(Text);
+            Program.config.save();
+        }
+    }
+}
