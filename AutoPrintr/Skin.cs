c7fde72d6c0ce007ab22e271fa40159da640312e
VoidVolker <voidvolker@gmail.com>
2016-08-29 16:15:18 +0300

    [+] New checkboxes

---
+++ AutoPrintr/Skin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using Newtonsoft.Json;
+using System.Windows.Forms;
+
+namespace AutoPrintr
+{
+    public class Skin
+    {
+        public skinWindow window;
+        public skinTable table;
+        public skinLog log;        
+    }
+    
+    public class skinTable
+    {
+        public Color BackColor;
+        public Font Font;
+        public Color ForeColor;
+        public Image BackgroundImage;
+        public ImageLayout BackgroundImageLayout;
+        public TableLayoutPanelCellBorderStyle CellBorderStyle;
+    }
+
+    public class skinWindow
+    {
+        public Color BackColor;
+        public Font Font;
+        public Color ForeColor;
+        public Image BackgroundImage;
+        public ImageLayout BackgroundImageLayout;
+        public FormBorderStyle FormBorderStyle;
+    }
+
+    public class skinLog
+    {
+        public Color BackColor;
+        public Font Font;
+        public Color ForeColor;
+    }
+
+    public class skinCheckBox
+    {
+        public Color BackColor;
+        public Color ForeColor;
+        public Font Font;
+        public Color BorderColor;
+    }
+
+}
