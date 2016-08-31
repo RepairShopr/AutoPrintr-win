d98ce6fba1d1b4a133a02d7749b5452ebbdddd28
VoidVolker <voidvolker@gmail.com>
2016-08-31 22:30:35 +0300

    [+] Log configuration and log level dropdown in UI

--- AutoPrintr/Properties/AssemblyInfo.cs
+++ AutoPrintr/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.371")]
+[assembly: AssemblyVersion("0.0.0.403")]
