c65dfa439e5e0a0a3635617d2da47f9208002712
VoidVolker <voidvolker@gmail.com>
2016-09-03 01:13:34 +0300

    [+] new icons, registers, fixed not loaded location bug, wrong username pass,one print job per line in UI, url removed

---
+++ AutoPrintr/RegisterDD.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace AutoPrintr
+{
+    /// <summary>
+    /// Custom combo box for selecting register associated with printer
+    /// </summary>
+    class RegisterDD : ComboBox
+    {
+        /// <summary>
+        /// Printer for this combo box
+        /// </summary>
+        public Printer printer;
+        Dictionary<string, int> rlist = new Dictionary<string, int>()
+        {
+             {"None", 0}
+        };
+        /// <summary>
+        /// Create new combo box for selected printer
+        /// </summary>
+        /// <param name="printer"></param>
+        public RegisterDD(Printer printer, List<LoginServer.Register> rlist)
+        {
+            this.printer = printer;
+
+            DropDownStyle = ComboBoxStyle.DropDownList;
+            Items.Add("None");
+            this.Text = "None";
+            foreach (LoginServer.Register r in rlist)
+            {
+                this.rlist.Add(r.name, r.id);
+                Items.Add(r.name);
+            }
+
+            // Setting handler for change event
+            this.SelectedIndexChanged += RegisterDD_TextChanged;
+
+            AutoSize = true;
+            Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top;
+        }
+
+        /// <summary>
+        /// Combo box change event handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void RegisterDD_TextChanged(object sender, EventArgs e)
+        {
+            printer.register = rlist[this.Text];    
+            Program.config.save();
+        }
+    }
+}
