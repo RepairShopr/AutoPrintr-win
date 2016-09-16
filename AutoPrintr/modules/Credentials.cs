1e0ea578e271a541e8f0ace16338d849335ded1b
VoidVolker <voidvolker@gmail.com>
2016-09-16 10:36:10 +0300

    [+] Credentials file

---
+++ AutoPrintr/modules/Credentials.cs
@@ -0,0 +1,13 @@
+﻿namespace AutoPrintr
+{
+    /// <summary>
+    /// Class for credentials storing
+    /// </summary>
+    class Credentials
+    {
+        /// <summary>
+        /// Pusher API key
+        /// </summary>
+        public const string SrvXT = "";
+    }
+}
